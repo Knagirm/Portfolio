3299af058359b71e298ac235fbb7f57f33a8457d
Knagirm <mrigank20.ash@gmail.com>
2021-06-27 00:40:25 +0530

    try

--- forms/contact.php
+++ forms/contact.php
@@ -17,7 +17,7 @@
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
-  
+
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
@@ -38,4 +38,13 @@
   $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
+  $receiving_email_address = 'mrigank20.ash@gmail.com';
+  $email_to = $receiving_email_address;
+  $name = $_POST['name'];
+  $email_from = $_POST['email'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
+  $formcontent = " From: $name \n Subject: $subject \n Message: $message";
+  mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+  echo "Thank You";
 ?>
